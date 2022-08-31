ef55f4229e760bd6931105d68bdd29af2b93dc7c
Kristjan Kosic <kristjan@protokol.com>
2022-08-31 13:21:18 +0000

    test: Vault cracked

--- test/Vault.t.sol
+++ test/Vault.t.sol
@@ -37,6 +37,22 @@ contract TestVault is BaseTest {
         vm.startPrank(player, player);
 
         /** CODE YOUR EXPLOIT HERE */
+        // of course on blockchain everything is public as stored in evm
+        // check this: https://solidity-by-example.org/hacks/accessing-private-data/
+        // looking at contract Vault we have two variables, one bool and then password
+        // with help
+
+        // That calldata is the same `bytes32 _password` used as password!
+        // You could fork the blockchain and inspect the storage slot of the contract via etherjs getStorageAt (https://docs.ethers.io/v5/api/providers/provider/#Provider-getStorageAt)
+        // In this case we are using Foundry so here's the implementation with the tool we're using
+
+        // In this case we are going to read from the second slot (index 1) because bytes32 take a whole word (256bits) so
+        // Position 0 -> `locked`
+        // Position 1 -> `password`
+
+        bytes32 password = vm.load(levelAddress, bytes32(uint256(1)));
+
+        level.unlock(password);
 
         assertEq(level.locked(), false);
 
